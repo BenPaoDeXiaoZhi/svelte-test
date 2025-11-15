3e8421505c81685f478fedaec35bc24db62fb27a
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-15 22:57:59 +0800

    更新 main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { mount } from 'svelte'
 import './app.css'
-import Root from './Root.svelte'
+import App from './App.svelte'
 
-const app = mount(Root, {
-  target: document.getElementById('root')!,
+const app = mount(App, {
+  target: document.getElementById('app')!,
 })
 
 export default app
