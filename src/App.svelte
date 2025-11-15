dd7a23d9f9caee93734fb2b7bdeb714ae1ed05b4
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-16 07:35:33 +0800

    更新 App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
-  import Banner from "./lib/Banner.svelte"
+  import Banner from "./lib/Banner.svelte";
+  import svelteIcon from.'./assets/svelte.svg'
 </script>
 
-<Banner --bg-color="#196090">file manager</Banner>
+<Banner --bg-color="#196090">
+<img src={svelteIcon} />
+file manager
+</Banner>
+
+<style>
+img {
+  height: 100%;
+  display: inline;
+}
+</style>
