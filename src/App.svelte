2387e4aab755d926eb9e8a07228e38bdfcd7f14b
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-16 09:04:51 +0800

    更新 App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Banner from "./lib/Banner.svelte";
+  import svelteIcon from './assets/svelteicon.svg'
   import FileDisplay from './lib/FileDisplay.svelte'
   let reslist=$state([])
 </script>
