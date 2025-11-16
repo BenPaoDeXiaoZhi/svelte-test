dba5fb4a058db6ceb7fb6f3e98629c84ebf3a956
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-16 08:32:45 +0800

    创建 Folder.svelte

---
+++ src/lib/Folder.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import folderIcon from '../assets/folder.svg'
+let { name } = $props()
+</script>
+
+<div class="container">
+  <img src={folderIcon} display="inline" height="70%">
+  <div class="name">
+    {name}
+  </div>
+</div>
+
+<style>
+.container {
+  width: 100%;
+  height: 1.5rem;
+  background-color: white;
+  border-style: solid;
+  border-color: grey;
+}
+.name {
+  font-size: 1rem;
+  color:black;
+}
+</style>
