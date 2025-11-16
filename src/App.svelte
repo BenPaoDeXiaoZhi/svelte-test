93b0930f637fde310ffcbd48c8c44d1e92f9ed57
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-16 08:46:16 +0800

    更新 App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   import Banner from "./lib/Banner.svelte";
-  import svelteIcon from './assets/svelte.svg'
+  import FileDisplay from './lib/FileDisplay.svelte'
+  let reslist=$state([])
 </script>
 
 <Banner --bg-color="#196090">
   <img src={svelteIcon} />
   <div>file manager</div>
 </Banner>
+<FileDisplay reslist={reslist}>
+<button onclick={(e)=>reslist.push({
+  type:"folder",
+  name:"abc"+reslist.length
+})}>
 
 <style>
   img {
