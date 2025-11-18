a0bc36d1d5fa139cf9c0188c03c6d3e3bf298af6
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2025-11-18 16:52:03 +0800

    ok

--- src/lib/FileObject.svelte
+++ src/lib/FileObject.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
+  import folderIcon from "../assets/folder.svg";
   import fileIcon from "../assets/file.svg";
-  let { name } = $props();
+  let {
+    name,
+    file = true,
+    onclick = function () {
+      console.log("not setted");
+    },
+  } = $props();
 </script>
 
-<div class="container">
-  <img src={fileIcon} height="70%" alt="folder" />
+<button class="container" {onclick} type="button">
+  <img src={file ? fileIcon : folderIcon} height="70%" alt="folder" />
   <div class="name">
     {name}
   </div>
-</div>
+</button>
 
 <style>
   .container {
     width: 100%;
     height: 1.5rem;
+    border: 0;
     border-top-style: solid;
     border-top-color: grey;
     border-top-width: 0.5px;
@@ -22,6 +30,7 @@
     align-items: center;
     padding: 0px 0.5rem;
     box-sizing: border-box;
+    cursor: pointer;
   }
   .name {
     font-size: 1rem;
